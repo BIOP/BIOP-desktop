3c6d637bc0d02762815dc68b2ff817389cbf41f4
romainGuiet <romain.guiet@epfl.ch>
2026-07-17 13:13:48 +0200

    modify start_samapi.sh to match pattern with base:0.30 and add samapi.desktop to icons folder

--- docker/icons/scripts/start_samapi.sh
+++ docker/icons/scripts/start_samapi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-source activate samapi
+set -e
+source /opt/conda/etc/profile.d/conda.sh
+conda activate samapi
 python -m uvicorn samapi.main:app --workers 2
 read -rsp $"Press enter to continue..."
