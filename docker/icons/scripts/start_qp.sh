a06bc687862d0fc6ad824aa1e1095c97fbedae7f
romainGuiet <romain.guiet@epfl.ch>
2026-08-18 08:54:08 +0200

    modify Dockerfile-djl (move to cu126 for GPU compatibility) , start_qp.sh , qp_prefs.xml

--- docker/icons/scripts/start_qp.sh
+++ docker/icons/scripts/start_qp.sh
@@ -1,4 +1,6 @@
-export PYTORCH_LIBRARY_PATH=/opt/conda/envs/djl/lib/python3.12/site-packages/torch/lib/
-export PYTORCH_VERSION=2.5.1
-export PYTORCH_FLAVOR=cpu
-/opt/QuPath/bin/QuPath -q
+#!/usr/bin/env bash
+export LD_LIBRARY_PATH=/opt/conda/envs/djl/lib:/opt/conda/envs/djl/lib/python3.12/site-packages/torch/lib:$LD_LIBRARY_PATH
+export PYTORCH_LIBRARY_PATH=/opt/conda/envs/djl/lib/python3.12/site-packages/torch/lib
+export PYTORCH_VERSION=2.7.1
+export PYTORCH_FLAVOR=cu126
+exec /opt/QuPath/bin/QuPath -q -Djna.library.path=/opt/conda/envs/djl/lib
